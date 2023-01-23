c76c36e59a0cf85b11729e01a2f7f56651e872c4
Jhon Useche <jhon.useche@telus.com>
2023-01-23 16:56:19 -0700

    Add loops to loop through the data.

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -3,7 +3,26 @@ require_relative './exercise_1'
 require_relative './exercise_2'
 require_relative './exercise_3'
 
-puts "Exercise 4"
-puts "----------"
+puts 'Exercise 4'
+puts '----------'
 
 # Your code goes here ...
+Store.create(name: 'Surrey', annual_revenue: 224_000, mens_apparel: false, womens_apparel: true)
+Store.create(name: 'Whistler', annual_revenue: 1_900_000, mens_apparel: true, womens_apparel: false)
+Store.create(name: 'Yaletown', annual_revenue: 430_000, mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true)
+
+@mens_stores.each do |store|
+  puts store.name
+  puts store.annual_revenue
+end
+
+@womens_stores = Store.where(womens_apparel: true)
+
+@womens_stores.each do |store|
+  if store.annual_revenue < 1_000_000
+    puts store.name
+    puts store.annual_revenue
+  end
+end
