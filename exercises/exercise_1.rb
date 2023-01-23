a06492371ced9c07a70e263a040efb80d37ed6cb
Jhon Useche <jhon.useche@telus.com>
2023-01-23 16:37:51 -0700

    Add put Store.count to count the number of stores in the database.

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -7,3 +7,5 @@ puts '----------'
 Store.create(name: 'Burnaby', annual_revenue: 300_000, mens_apparel: true, womens_apparel: true)
 Store.create(name: 'Richmond', annual_revenue: 1_260_000, mens_apparel: false, womens_apparel: true)
 Store.create(name: 'Gastown', annual_revenue: 190_000, mens_apparel: true, womens_apparel: false)
+
+puts Store.count
