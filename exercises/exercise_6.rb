e9e61030e56e8d63b1b1d890dace6ad193a3d246
Jhon Useche <jhon.useche@telus.com>
2023-01-23 17:36:11 -0700

    Create new employees.

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -5,7 +5,13 @@ require_relative './exercise_3'
 require_relative './exercise_4'
 require_relative './exercise_5'
 
-puts "Exercise 6"
-puts "----------"
+puts 'Exercise 6'
+puts '----------'
 
 # Your code goes here ...
+@store1.employees.create(first_name: 'Jhon', last_name: 'Useche', hourly_rate: 70)
+@store1.employees.create(first_name: 'Mike', last_name: 'Lone', hourly_rate: 75)
+@store1.employees.create(first_name: 'Nick', last_name: 'Useche', hourly_rate: 60)
+@store2.employees.create(first_name: 'Isabel', last_name: 'Smith', hourly_rate: 65)
+@store2.employees.create(first_name: 'Homer', last_name: 'Simpsons', hourly_rate: 80)
+@store2.employees.create(first_name: 'Terry', last_name: 'Smith', hourly_rate: 70)
